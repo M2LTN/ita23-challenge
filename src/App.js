be905825a559f7361fb04849881d09e8b2aaa309
M2LTN <aaron.malton@voco.ee>
2025-04-02 12:22:10 +0300

    Header ja meals added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from './components/Header'
+import Meals from './components/Meals'
 const App = () => {
  return (
-    <>
+    <div id='root'>
      <h1>Food Order App</h1>
-    </>
+    <Header />
+    <Meals />
+    </div>
   );
 }
 
