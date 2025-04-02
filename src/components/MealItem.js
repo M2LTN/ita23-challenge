f6206e18fa4236a822f784aa4e159534d5396acb
M2LTN <aaron.malton@voco.ee>
2025-04-02 13:38:01 +0300

    Valuuta andmete kuvamine

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,12 @@
 const MealItem = (props) => {
+    const europrice = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(props.meal.price);
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3 className="">{props.meal.name}</h3>
-                    <p className="meal-item-price">{props.meal.price}</p>
+                    <p className="meal-item-price">{europrice}</p>
                     <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p>
